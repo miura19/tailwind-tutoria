049847da3350ebfd5d37563be931615cad68adec
miura19 <littlecloud194@gmail.com>
2023-10-30 20:54:05 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+  fontFamily: {
+    poppins: ['Poppins', 'sans-serif'],
+  },
+  extend: {
+    colors: {
+      body: "#050505",
+    }
+  },
+};
+export const plugins = [];
+
